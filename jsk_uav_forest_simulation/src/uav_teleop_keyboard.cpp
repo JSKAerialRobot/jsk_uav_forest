f30d260486b2a82d700d2ec2e61e211a2374e640
Tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2017-04-09 01:14:36 +0900

    Comment "include" dir from the header dependency. Add definication of ros_distro for version difference in keyboard file

--- jsk_uav_forest_simulation/src/uav_teleop_keyboard.cpp
+++ jsk_uav_forest_simulation/src/uav_teleop_keyboard.cpp
@@ -39,7 +39,11 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Float64.h>
+#include <cstdlib>
+
+#ifdef kinetic
 #include <hector_uav_msgs/EnableMotors.h>
+#endif
 
 #define KEYCODE_A 0x61
 #define KEYCODE_D 0x64
@@ -70,7 +74,9 @@ private:
   ros::NodeHandle n_;
   ros::Publisher vel_pub_;
   ros::ServiceClient motor_on_client_;
+#ifdef kinetic
   hector_uav_msgs::EnableMotors srv_;
+#endif
 
 public:
   void init()
@@ -78,15 +84,16 @@ public:
     cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
 
     vel_pub_ = n_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+#ifdef KINETIC
+    srv_.request.enable = true;
     motor_on_client_ = n_.serviceClient<hector_uav_msgs::EnableMotors>("/enable_motors");
+#endif
     ros::NodeHandle n_private("~");
     n_private.param("walk_vel", walk_vel, 1.0);
     n_private.param("run_vel", run_vel, 4.0);
     n_private.param("yaw_rate", yaw_rate, 1.0);
     n_private.param("yaw_run_rate", yaw_rate_run, 1.5);
     n_private.param("vertical_vel", vertical_vel, 1.0);
-
-    srv_.request.enable = true;
   }
 
   ~TeleopUAVKeyboard()   { }
@@ -178,7 +185,9 @@ void TeleopUAVKeyboard::keyboardLoop()
           dirty = true;
           break;
         case KEYCODE_P:
+#ifdef kinetic
           motor_on_client_.call(srv_);
+#endif
           cmd.linear.z = vertical_vel;
           dirty = true;
           break;

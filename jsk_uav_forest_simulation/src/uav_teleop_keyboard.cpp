a5757f2aed96217d2b84d3c88b50b4e4853e4be1
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2017-04-09 10:04:27 +0900

    Change the hector related repository to indigo-devel version, even in {$ROS_DISTRO} == kinect

--- jsk_uav_forest_simulation/src/uav_teleop_keyboard.cpp
+++ jsk_uav_forest_simulation/src/uav_teleop_keyboard.cpp
@@ -39,9 +39,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Float64.h>
-#ifdef kinetic
-#include <hector_uav_msgs/EnableMotors.h>
-#endif
 
 #define KEYCODE_A 0x61
 #define KEYCODE_D 0x64
@@ -71,10 +68,6 @@ private:
   geometry_msgs::Twist cmd;
   ros::NodeHandle n_;
   ros::Publisher vel_pub_;
-#ifdef kinetic
-  ros::ServiceClient motor_on_client_;
-  hector_uav_msgs::EnableMotors srv_;
-#endif
 
 public:
   void init()
@@ -82,10 +75,6 @@ public:
     cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
 
     vel_pub_ = n_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-#ifdef kinetic
-    srv_.request.enable = true;
-    motor_on_client_ = n_.serviceClient<hector_uav_msgs::EnableMotors>("/enable_motors");
-#endif
     ros::NodeHandle n_private("~");
     n_private.param("walk_vel", walk_vel, 1.0);
     n_private.param("run_vel", run_vel, 4.0);
@@ -183,9 +172,6 @@ void TeleopUAVKeyboard::keyboardLoop()
           dirty = true;
           break;
         case KEYCODE_P:
-#ifdef kinetic
-          motor_on_client_.call(srv_);
-#endif
           cmd.linear.z = vertical_vel;
           dirty = true;
           break;

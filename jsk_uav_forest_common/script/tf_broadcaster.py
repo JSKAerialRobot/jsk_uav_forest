d72cb3d55e63ce52ecd1fb2f05ce887e610928af
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2017-04-25 04:22:01 +0900

    Tf broadcaster: split the relationshop from world frame to laser frame into two part. world -> base, and base -> laser

--- jsk_uav_forest_common/script/tf_broadcaster.py
+++ jsk_uav_forest_common/script/tf_broadcaster.py
@@ -6,15 +6,39 @@ import tf
 import numpy as np
 from nav_msgs.msg import Odometry
 
-def odometryCallback(msg):
-    br = tf.TransformBroadcaster()
-    pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z])
-    ori = np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-    br.sendTransform(pos, ori, msg.header.stamp, "/uav", "/world")
-    br.sendTransform(pos, ori, msg.header.stamp, "/laser", "/world")
+class odom_tf:
+    def init(self):
+        self.uav_odom_topic_name = rospy.get_param("~uav_odom_topic_name", "/modified_odom")
+        self.use_mapping = rospy.get_param("~use_mapping", False)
+        self.map_frame_name = rospy.get_param("~map_frame_name", "/map")
+        self.world_frame_name = rospy.get_param("~world_frame_name", "/world")
+        self.base_frame_name = rospy.get_param("~base_name", "/base_link")
+        self.base_footprint_frame_name = rospy.get_param("~base_footprint_name", "/base_footprint")
+        self.laser_frame_name = rospy.get_param("~laser_frame_name", "/laser")
+
+        rospy.Subscriber(self.uav_odom_topic_name, Odometry, self.odometryCallback)
+
+        self.map_init = False
+
+    def odometryCallback(self, msg):
+        br = tf.TransformBroadcaster()
+
+        pos = np.array([msg.pose.pose.position.x, msg.pose.pose.position.y, 0])
+        ori = np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+        euler = tf.transformations.euler_from_quaternion(ori)
+        quaternion = tf.transformations.quaternion_from_euler(0, 0, euler[2])
+
+        # /world -> /base
+        br.sendTransform(pos, quaternion, msg.header.stamp, self.base_footprint_frame_name, self.world_frame_name)
+        pos = np.array([0, 0, msg.pose.pose.position.z])
+        quaternion = tf.transformations.quaternion_from_euler(euler[0], euler[1], 0)
+        br.sendTransform(pos, quaternion, msg.header.stamp, self.base_frame_name, self.base_footprint_frame_name)
+        br.sendTransform(pos, quaternion, msg.header.stamp, self.laser_frame_name, self.base_footprint_frame_name)
+
+
 
 if __name__ == '__main__':
     rospy.init_node("tf_broadcaster")
-    uav_odom_topic_name = rospy.get_param("~uav_odom_topic_name", "/modified_odom")
-    rospy.Subscriber(uav_odom_topic_name, Odometry, odometryCallback)
+    odomTF = odom_tf()
+    odomTF.init()
     rospy.spin()

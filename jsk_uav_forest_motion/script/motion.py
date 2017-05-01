4060cb04176c2397b6b24f0c6f5a2002dafc8f12
tomokianzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2017-05-01 12:52:34 +0900

    fix bug: uav goes to beginning position in circle motion finish state

--- jsk_uav_forest_motion/script/motion.py
+++ jsk_uav_forest_motion/script/motion.py
@@ -327,10 +327,11 @@ class ForestMotion:
             vel_msg = self.goCircle(self.tree_xy_pos_, self.takeoff_height_ + self.circle_motion_count_ * self.circle_motion_height_step_, self.circle_y_vel_, self.circle_radius_)
         if self.state_machine_ == self.FINISH_CIRCLE_MOTION_STATE_:
             tree_direction = math.atan2(self.tree_xy_pos_[1], self.tree_xy_pos_[0])
-            vel_msg = self.goPos(self.LOCAL_FRAME_, np.array([self.tree_xy_pos_[0] - self.circle_radius_ * math.cos(tree_direction),
-                                                              self.tree_xy_pos_[1] - self.circle_radius_ * math.sin(tree_direction)]),
+            vel_msg = self.goPos(self.LOCAL_FRAME_, np.array([self.tree_xy_pos_[0] - self.circle_radius_,
+                                                              self.tree_xy_pos_[1]]),
                                  self.takeoff_height_,
                                  self.circle_initial_yaw_)
+
         if self.state_machine_ == self.TURN_STATE_:
             vel_msg = self.goPos(self.GLOBAL_FRAME_, self.turn_uav_xy_pos_, self.takeoff_height_, self.initial_yaw_ + math.pi)
         if self.state_machine_ == self.RETURN_HOME_STATE_:

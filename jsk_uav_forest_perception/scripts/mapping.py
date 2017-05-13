f8beacecf62b9afbe41b5f93d5bab79dc960f5fa
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2017-05-14 08:18:39 +0900

    Write the tf from map_frame to odom_frame into the map yaml file to broadcast tf in the debug mode

--- jsk_uav_forest_perception/scripts/mapping.py
+++ jsk_uav_forest_perception/scripts/mapping.py
@@ -2,6 +2,7 @@
 
 import time
 import sys
+import os
 import rospy
 import math
 import signal
@@ -10,11 +11,15 @@ from geometry_msgs.msg import PointStamped
 from std_msgs.msg import Bool
 import subprocess
 from datetime import datetime
+from std_msgs.msg import Bool
+import tf
 
 class Mapping:
     def init(self):
         rospy.init_node('mapping', anonymous=True)
         self.mapping = False
+        self.map_frame = rospy.get_param("~map_frame", "/map")
+        self.odom_frame = rospy.get_param("~odom_frame", "/world")
         self.first_tree_dist_thresh = rospy.get_param("~first_tree_dist_thresh", 3) # [m]
         self.tree_location_sub_topic_name = rospy.get_param("~tree_location_sub_topic_name", "tree_location")
         self.mapping_control_sub_topic_name = rospy.get_param("~mapping_control_sub_topic_name", "/tracking_control")
@@ -40,12 +45,35 @@ class Mapping:
             rospy.loginfo("Restart mapping");
         else:
             rospy.logwarn("Stop mapping");
-            # save map
+
+            # get time 
             dt_obj = datetime.fromtimestamp(rospy.get_time())
             date_str = dt_obj.strftime("%Y-%m-%d-%H-%M")
+
+            # get tf
+            trans = None
+            rot = None
+            listener = tf.TransformListener()
+            while not rospy.is_shutdown():
+                try:
+                    (trans,rot) = listener.lookupTransform(self.map_frame, self.odom_frame, rospy.Time(0))
+                    rospy.logwarn("get tf from %s to %s", self.map_frame, self.odom_frame)
+                    break
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    #rospy.logerr("can not get tf from %s to %s", self.map_frame, self.odom_frame)
+                    continue
+
+            # save map
             self.saving_process = subprocess.Popen(["rosrun", "map_server", "map_saver", "-f", date_str])
+
             time.sleep(1)
             self.mapping_process.send_signal(signal.SIGINT)
+
+            #save tf
+            f = open(os.environ['HOME'] + '/.ros/' + date_str + '.yaml', 'a')
+            f.write('trans: [' + str(trans[0]) + ', '  + str(trans[1]) + ', ' + str(trans[2]) +']\n')
+            f.write('rot: [' + str(rot[0]) + ', '  + str(rot[1]) + ', ' + str(rot[2]) + ', ' + str(rot[3]) + ']\n')
+            f.close()
             self.mapping = False;
 
 if __name__ == '__main__':

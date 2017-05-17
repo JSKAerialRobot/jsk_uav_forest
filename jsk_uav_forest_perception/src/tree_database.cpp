0f99cf702df79fe81ab22058f54dbf18e0332af8
Tomoki Anzai <anzai@jsk.imi.i.u-tokyo.ac.jp>
2017-05-17 20:35:06 +0900

    Update the tree database update rult: 1. single threshold to judge whether to add new tree or update existing trees. 2. do not update the existing trees if the point is closed to more than two trees within a certain range

--- jsk_uav_forest_perception/src/tree_database.cpp
+++ jsk_uav_forest_perception/src/tree_database.cpp
@@ -30,8 +30,7 @@ bool operator>(const TreeHandlePtr& left, const TreeHandlePtr& right)
 TreeDataBase::TreeDataBase(ros::NodeHandle nh, ros::NodeHandle nhp):nh_(nh), nhp_(nhp)
 {
   trees_.resize(0);
-  nhp_.param("min_distance", min_distance_, 1.0); // 1.0[m]
-  nhp_.param("max_radius", max_radius_, 0.5); // 1.0[m]
+  nhp_.param("tree_margin_radius", tree_margin_radius_, 1.0); // 1.0[m]
   nhp_.param("valid_num", valid_num_, 7);
   nhp_.param("verbose", verbose_, false);
   nhp_.param("visualization_marker_topic_name", visualization_marker_topic_name_, string("/visualization_marker"));
@@ -47,6 +46,7 @@ void TreeDataBase::add(const TreeHandlePtr new_tree)
 bool TreeDataBase::updateSingleTree(const tf::Vector3& tree_pos, const double& tree_radius, const bool only_target)
 {
   bool new_tree = true;
+  bool do_update = true;
   float min_dist = 1e6;
   TreeHandlePtr target_tree;
   int tree_index = 0;
@@ -56,12 +56,15 @@ bool TreeDataBase::updateSingleTree(const tf::Vector3& tree_pos, const double& t
       size_t index = distance(trees_.begin(), it);
 
       /* we assume that the distance of any two trees is more than min_ditance_ */
-      if(dist < min_distance_)
+      if(dist < tree_margin_radius_)
 	{
 	  if(verbose_)
             {
               if(!new_tree)
-                ROS_WARN("there are two trees which are to close to each other");
+                {
+                  ROS_WARN("there are two trees which are to close to each other");
+                  do_update = false;
+                }
             }
 	  new_tree = false;
 	}
@@ -76,27 +79,27 @@ bool TreeDataBase::updateSingleTree(const tf::Vector3& tree_pos, const double& t
 	}
     }
 
-  if(min_dist < max_radius_)
+  /* add new tree if necessary */
+  if(new_tree)
     {
-      /* update the global pos of the tree */
-      target_tree->updatePos(tree_pos,false);
-      target_tree->setRadius(tree_radius);
-      if(verbose_) cout << "Database tree No." << tree_index << ": update, small diff:" << min_dist << endl;
-      return true;
+      if(!only_target)
+        {
+          TreeHandlePtr new_tree = TreeHandlePtr(new TreeHandle(nh_, nhp_, tree_pos));
+          new_tree->setRadius(tree_radius);
+          add(new_tree);
+          return true;
+        }
     }
   else
     {
-      if(verbose_  && !only_target)
-	ROS_WARN("Database tree No.%d, lost the target tree, drift: %f, the nearest target location: [%f, %f, %f], prev target location: [%f, %f, %f]", tree_index, min_dist, target_tree->getPos().x(), target_tree->getPos().y(), target_tree->getPos().z(), tree_pos.x(), tree_pos.y(), tree_pos.z());
-    }
-
-  /* add new tree if necessary */
-  if(new_tree && !only_target)
-    {
-      TreeHandlePtr new_tree = TreeHandlePtr(new TreeHandle(nh_, nhp_, tree_pos));
-      new_tree->setRadius(tree_radius);
-      add(new_tree);
-      return true;
+      if(do_update)
+        {
+          /* update the global pos of the tree */
+          target_tree->updatePos(tree_pos,false);
+          target_tree->setRadius(tree_radius);
+          if(verbose_) cout << "Database tree No." << tree_index << ": update, small diff:" << min_dist << endl;
+          return true;
+        }
     }
 
   return false;
